cd0513498b55857921ec12c29c0263c8a5704a94
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-04-19 13:11:13 +0200

    add comment

--- src/main/java/se/lexicon/mail/api/MailService.java
+++ src/main/java/se/lexicon/mail/api/MailService.java
@@ -2,18 +2,16 @@ package se.lexicon.mail.api;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
-import java.util.UUID;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 @Component
 public class MailService {
@@ -21,15 +19,17 @@ public class MailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    void sendEmailWithAttachment(String to, String body, String subject) throws MessagingException, IOException {
-
+    void sendEmailWithAttachment(final String to, final String body, final String subject) throws MessagingException, IOException {
+        // Multipurpose Internet Mail Extensions (MIME)
+        // It represents a MIME style email message
         MimeMessage msg = javaMailSender.createMimeMessage();
 
         // true = multipart message
+        // Multipart message contains both plain text and HTML messages
         MimeMessageHelper helper = new MimeMessageHelper(msg, true);
 
         helper.setTo(to);
-        helper.setSubject(subject);
+        helper.setSubject(subject != null ? subject : "");
 
         // default = text/plain
         //helper.setText("Check attachment for image!");
